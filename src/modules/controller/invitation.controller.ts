a4978663a8f92eb6334cbab22d07fb246b01874e
sanja avi <avi.sanja@gmail.com>
2020-09-17 11:19:26 +0700

    Feature/Invitation and History
    - feat() Create Invitation & History
    
    - feat() Update Invitation & Create a new history
    
    - feat() Delete Invitation & Create a new history
    
    - fix() Change deleted status to "True" while invitation deleted
    
    - fix() invitation schema status enum
    
    - fix() While history created, actions will filled according to
      invitations conditions (invitations CRUD action)

--- src/modules/controller/invitation.controller.ts
+++ src/modules/controller/invitation.controller.ts
@@ -1,5 +1,5 @@
 import {Controller,Get,Post,Body,Put, Delete,Param} from '@nestjs/common';
-import {InvitationService,userService,OrgService} from '../services/invitation.service';
+import {InvitationService} from '../services/invitation.service';
 import {HistoryService} from '../services/history.service';
 import {invitation,users,organizations} from '../models/invitation.interface';
 import {history} from '../models/history.interface';
@@ -8,27 +8,27 @@ import {history} from '../models/history.interface';
 export class InvitationController{
   constructor(
     private readonly historyService:HistoryService,
-    private readonly invitationService:InvitationService,
-    private readonly UserService:userService,
-    private readonly orgService:OrgService){}
+    private readonly invitationService:InvitationService
+   /* private readonly UserService:userService,
+    private readonly orgService:OrgService*/){}
   @Post()
 
    async create(@Body() inv:invitation,hist:history){
     const invitation = await this.invitationService.createInvitation(inv);
     const invId = invitation._id
     const history = await this.historyService.createHistory(invId);
-    const userId = invitation.userId
+    //const userId = invitation.userId
     const organizationId = invitation.organizationId
-    const users =await this.UserService.findUser(userId)
-    const organization = await this.orgService.findOrg(organizationId);
-    await users.organizationsId.push(organization._id)
-    await organization.invitationId.push(invitation._id)
-    await users.invitationsId.push(invitation._id)
+    //const users =await this.UserService.findUser(userId)
+    //const organization = await this.orgService.findOrg(organizationId);
+    //await users.organizationsId.push(organization._id)
+    //await organization.invitationId.push(invitation._id)
+    //await users.invitationsId.push(invitation._id)
     await invitation.histories.push(history._id)
     //await history.invitationId.push(invId)
     //console.log(organization._id)
-    await organization.save()
-    await users.save()
+    //await organization.save()
+    //await users.save()
     await history.save()
     return invitation.save()
    }
@@ -45,12 +45,12 @@ export class InvitationController{
    	@Param('id') id,
    	@Body() Invitation:invitation,hist:history){
     const invitationId = await this.invitationService.findById(id);
-    const userId = invitationId.userId
-    const orgId = invitationId.organizationId
+    //const userId = invitationId.userId
+   // const orgId = invitationId.organizationId
     const history = await this.historyService.updateHistory(id)
     await invitationId.histories.push(history._id)
-    await this.UserService.pushUser(userId,id)
-    await this.orgService.pushOrg(orgId,id)
+    //await this.UserService.pushUser(userId,id)
+    //await this.orgService.pushOrg(orgId,id)
     await history.save()
     await invitationId.save()
     return await this.invitationService.updateInv(id,Invitation)
@@ -58,22 +58,23 @@ export class InvitationController{
    @Delete('/:id')
    async Delete(
      @Param('id') id,
-     @Body() Invitation:invitation,hist:history,org:organizations){
-    const invitations = await this.invitationService.findInv(id);
+     @Body() Invitation:invitation,hist:history){
+    const invitations = await this.invitationService.findById(id);
     //const invId = invitations._id
     const history = await this.historyService.deleteHistory(id)
-    const orgId = invitations.organizationId
-    const userId = invitations.userId
-    const organizationId = await this.orgService.findOrg(orgId);
-    await this.orgService.pullOrg(orgId,id);
-    await this.UserService.pullUser(userId,id);
-    await invitations.histories.push(history._id)
-    const histories = await this.invitationService.deleteInv(id,Invitation)
+   // const orgId = invitations.organizationId
+   // const userId = invitations.userId
+    //const organizationId = await this.orgService.findOrg(orgId);
+   // await this.orgService.pullOrg(orgId,id);
+  //  await this.UserService.pullUser(userId,id);
+  //  await invitations.histories.push(history._id)
+    
     //await pullOrg.save()
     //await pullUser.save()
-    await histories.save()
+    await invitations.histories.push(history._id)
     await invitations.save()
-    return await history.save()
+    await history.save()
+    return await this.invitationService.deleteInv(id,Invitation)
      
    }
 }
